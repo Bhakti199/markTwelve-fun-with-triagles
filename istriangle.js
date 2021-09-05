32ab775a3c38a563db322cf51706c01839398b3b
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2021-09-05 16:34:03 +0530

    Create istriangle.js

---
+++ istriangle.js
@@ -0,0 +1,26 @@
+const anglesEntered =document.querySelectorAll("#angle-input");
+const checkButton = document.querySelector("#check-button");
+const displayMessage = document.querySelector("#show-message");
+
+function isTriangleOrNot(){
+  
+    if(Number(anglesEntered[0].value) > 0 && Number(anglesEntered[1].value) > 0 && Number(anglesEntered[2].value) >0)
+{       const resultSum = calculateSumOfAnglesOfTriangle(Number(anglesEntered[0].value) , Number(anglesEntered[1].value), Number(anglesEntered[2].value));
+        
+                if(resultSum === 180){
+                    displayMessage.innerText = "Yay! The angles entered forms a triangle 🎉🎉"
+                }else{
+                    displayMessage.innerText = "The angles entered does not form a triangle 💔";
+                }
+
+   }else{
+        displayMessage.innerText="Please, Enter valid inputs.";
+   }
+}
+
+function calculateSumOfAnglesOfTriangle(angle1, angle2, angle3){
+    const  sumOfAngles =  angle1 + angle2 + angle3;
+    return sumOfAngles;
+}
+
+checkButton.addEventListener("click", isTriangleOrNot);
