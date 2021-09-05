0028feec56374eba6f5dcee0ed0f25bcd40702d7
Bhakti199 <78597483+Bhakti199@users.noreply.github.com>
2021-09-05 16:33:51 +0530

    Create hypotenuse.js

---
+++ hypotenuse.js
@@ -0,0 +1,24 @@
+const calculateBtn = document.querySelector("#button-calculate");
+const sideInputs = document.querySelectorAll(".side-input");
+const messageDisplay = document.querySelector("#show-message");
+
+function sumOfSquareOfSides( side1 , side2){
+        const counter = side1 * side1 + side2 * side2 ;
+        return counter; 
+}
+
+function calculateHypotenuse(){
+    const a = Number(sideInputs[0].value);
+    const b = Number(sideInputs[1].value);
+    if(a > 0 && b > 0)
+    {
+        const sumOfSidesSq = sumOfSquareOfSides(Number(sideInputs[0].value), Number(sideInputs[1].value));
+        const resultFinal = Math.sqrt(sumOfSidesSq);
+        messageDisplay.innerText ="The value of Hypotenuse B is : " +  resultFinal ;
+       
+    }else{
+        messageDisplay.innerText ="Please, Enter valid inputs";
+    }
+}
+
+calculateBtn.addEventListener("click" , calculateHypotenuse);
